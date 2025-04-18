57d7a0c127e38dd78c2397f304b4e0db4113ed1b
LiamHayden <Leftbehind1995@hotmail.com>
2025-04-18 22:29:22 +0100

    Intial commit + some

---
+++ OneDrive/Documents/web-projects/fitnessWebsite/fitnessWebsite/src/main/java/com/example/fitnessWebsite/FitnessWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitnessWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessWebsiteApplication.class, args);
+	}
+
+}
---
+++ OneDrive/Documents/web-projects/fitnessWebsite/fitnessWebsite/src/test/java/com/example/fitnessWebsite/FitnessWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitnessWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
